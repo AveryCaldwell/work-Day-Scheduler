a0173b8ecd3c0128c18abf9f85948c3a68e10483
Avery <averycaldwell7@gmail.com>
2022-12-11 19:11:43 -0800

    updated comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-// This function renders all the elements in the html of the browser before running the code
+// This function runs after the rendering of all the elements in the html of the browser.
 $(document).ready(function () {
   // Statement of ordinals after currentDay in the header.
   let ordinal;
@@ -34,17 +34,18 @@ $(document).ready(function () {
 });
 // Gets today's date with Day.js.
 let now = dayjs();
-//  Returns the hour (9 to 17) of a date.
+//  Returns the hour (Does not have to be 9am-5pm[17]) of a date.
 let curHour = now.hour();
 
 // Array of times of day by hour (24hr).
 const hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+// Sets the class (past, present, future) of the hour entries of the day planner which in turn sets the color of the entries.
 const setColor = function (hour) {
   let hourEl = "#hour-" + hour;
   // Removes the past, present, and future classes.
   $(hourEl).removeClass("past").removeClass("present").removeClass("future");
-  // Compares the id to the current hour and applies corresponding attributes.
+  // Compares the id to the current hour and applies corresponding class.
   if (hour < curHour) {
     $(hourEl).addClass("past");
   } else if (hour === curHour) {
@@ -53,22 +54,22 @@ const setColor = function (hour) {
     $(hourEl).addClass("future");
   }
 };
-// A listener for click events on the save button.
-const addListener = function (hour) {
-  let hourEl = "#hour-" + hour;
-  $(hourEl).children("button").on("click", save);
-};
-// This function gets user input that is saved in localStorage.
-const loadData = function (hour) {
-  let hourEl = "#hour-" + hour;
-  const textEl = $(hourEl).children("textarea")[0];
-  textEl.value = localStorage.getItem("hour-" + hour);
-};
-// This function sets the value of corresponding text areas.
+// This function stores the value of corresponding text areas in local storage.
 const save = function () {
   // `this` is the element you click on.
   const parentEl = this.parentElement;
   const textEl = $(parentEl).children("textarea")[0];
   const value = textEl.value;
   localStorage.setItem(parentEl.id, value);
+};
+// A listener for click events on the save button.
+const addListener = function (hour) {
+  let hourEl = "#hour-" + hour;
+  $(hourEl).children("button").on("click", save);
+};
+// This function retrieves user input stored in localStorage, and sets the value of the corresponding textarea for the hour.
+const loadData = function (hour) {
+  let hourEl = "#hour-" + hour;
+  const textEl = $(hourEl).children("textarea")[0];
+  textEl.value = localStorage.getItem("hour-" + hour);
 };
